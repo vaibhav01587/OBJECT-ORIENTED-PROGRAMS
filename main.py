22756e8036b980bfddd9d5f396424e8f18c0ee40
vaibhav01587 <vaibhavverma01587@gmail.com>
2025-04-28 11:17:13 +0530

    HOW PACKAGES WORKS
    I UPLOAD  CONCEPT OF PACKAGES IN PYTHONS EXPLAIN WITH CODE

---
+++ main.py
@@ -0,0 +1,62 @@
+# #First Syntax calling
+# import Admin.service,Admin.product
+# Admin.service.admin_service() # last one is function name
+
+# Admin.product.admin_product()
+
+# import Admin.Common.header
+# Admin.Common.header.common_header()
+# import Admin.Common.footer
+# Admin.Common.footer.common_footer() 
+# import Tech.profile
+# Tech.profile.tech_profile()
+# import Tech.work
+# Tech.work.tech_work()
+
+# # second method how to use packages
+# from Admin import service
+# service.admin_service()
+# from Admin import product
+# product.admin_product()
+
+# from Admin.Common import header
+# header.common_header()
+# from Admin.Common import footer
+# footer.common_footer()
+
+# third method how to import pacakages to function
+
+# from Admin.service import admin_service
+# admin_service()
+
+# from Admin.product import admin_product
+# admin_product()
+
+# fourth method how to import packages to function nested packages
+# from Admin.Common.header import common_header
+# common_header()
+
+# from User.profile import user_profile
+# user_profile()
+# from User.request import user_request
+# user_request()
+
+# from Admin import service,product 
+# service.admin_service()
+# product.admin_product()
+
+# from Admin import *
+# service.admin_service()
+# product.admin_product()
+
+# from Admin.Common import header,footer
+# header.common_header()
+# footer.common_footer()
+
+# from Admin.Common import*
+# from Admin import *
+# service.admin_service()
+# product.admin_product()
+# header.common_header()
+# footer.common_footer()
+
